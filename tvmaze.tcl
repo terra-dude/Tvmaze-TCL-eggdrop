4228f6aac46f11d93776e340250b42c0a89137c4
terra-dude <59861614+terra-dude@users.noreply.github.com>
2023-01-28 10:01:10 +0100

    Update tvmaze.tcl
    Imp/Fix: Endline / Removed Spaces

--- tvmaze.tcl
+++ tvmaze.tcl
@@ -1,7 +1,7 @@
 ################################################
 # This Script is writed by TeRRaNoVA           #
 # Tvmaze today's schedule script               #
-# 2023-01-29 v.1.0.1 Beta                      #
+# 2023-01-29 v.1.0.2 Beta                      #
 #                                              #
 # CHANGELOG:---------------------------------- #
 # Fixed: Clock/date now gets correct date      #
@@ -9,6 +9,7 @@
 # Update: trigger from !tv to !today           # 
 # Improvement: Now shows Network if NULL       #
 # Improvement: Skip Words                      #
+# Imp/Fix: Endline / Removed Space             #
 ################################################
 
 #### Package needed
@@ -29,10 +30,10 @@ proc pub:announce {nick host handle chan arg} {
     set count 0
     set announcement_count 0
     set skip_words { "Episode" "0 and 2."}
-    putquick "PRIVMSG $chan :  Here is the TV schedule for today:"
+    putquick "PRIVMSG $chan : Here is the TV schedule for today US:"
     foreach show $shows {
         if {$announcement_count == 10} {
-    putquick "PRIVMSG $chan :  Hold on there is more......"
+    putquick "PRIVMSG $chan : Hold on there is more......"
             after 5000
             set announcement_count 0
         }
@@ -48,13 +49,13 @@ proc pub:announce {nick host handle chan arg} {
         set number [dict get $show "number"]
         set time [dict get $show "airtime"]
         if {[lsearch -exact $skip_words $name] == -1} {
-   putquick "PRIVMSG $chan :  $name \(S$season E$number\) airs at $time on $network"
+   putquick "PRIVMSG $chan : $name \(S$season/E$number\) airs at $time on $network"
         incr count
             incr announcement_count
         }
     }
 }
-
-    }
 }
-putlog "Loaded tvmaze todays schedule script v1.0.1 Beta"
+putquick "PRIVMSG $chan : End of TV schedule list for today $date. Checking for new shows tomorrow......
+}
+putlog "Loaded tvmaze todays schedule script v1.0.2 Beta"
